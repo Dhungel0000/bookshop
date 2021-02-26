643f05efd02f1945e132b4fdd8241d2becc96e72
ADHUNGEL <Aashrika.Dhungel@otto.de>
2021-02-26 11:19:57 +0100

    count non static

--- src/main/java/bookshop/model/OrderItem.java
+++ src/main/java/bookshop/model/OrderItem.java
@@ -1,48 +1,34 @@
 package bookshop.model;
 
-public class OrderItem
-{
-	private static int count;
+public class OrderItem {
+    private final int count;
 
-	private Product product;
+    private final Product product;
 
-	public OrderItem(Product product, int count)
-	{
-		this.product = product;
-		this.count = count;
-	}
+    public OrderItem(Product product, int count) {
+        this.product = product;
+        this.count = count;
+    }
 
-	public int getCount()
-	{
-		return count;
-	}
+    public int getCount() {
+        return count;
+    }
 
-	public void setCount(int count)
-	{
-		this.count = count;
-	}
 
-	public Product getProduct()
-	{
-		return product;
-	}
+    public Product getProduct() {
+        return product;
+    }
 
-	public void setProduct(Product product)
-	{
-		this.product = product;
-	}
 
-	@Override
-	public boolean equals(Object obj)
-	{
-		OrderItem other = (OrderItem) obj;
-		return other.product.equals(this.product)
-				&& other.count == this.count;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        OrderItem other = (OrderItem) obj;
+        return other.product.equals(this.product)
+                && other.count == this.count;
+    }
 
-	@Override
-	public int hashCode()
-	{
-		return this.count;
-	}
+    @Override
+    public int hashCode() {
+        return this.count;
+    }
 }
