ba094abdc22e2852bf13fa0b73d9b037b1387f59
ADHUNGEL <Aashrika.Dhungel@otto.de>
2021-02-26 12:28:22 +0100

    if else removed with simple stream

--- src/main/java/bookshop/processes/OrderCalculator.java
+++ src/main/java/bookshop/processes/OrderCalculator.java
@@ -8,34 +8,18 @@ import java.math.RoundingMode;
 
 public class OrderCalculator
 {
+	private BigDecimal total = BigDecimal.ZERO;
 	public String getTotalFor(Order order)
 	{
-		BigDecimal total = BigDecimal.ZERO;
-		if (order.getItems().size() > 0)
-		{
-			total = addOrderItem(total, order.getItems().get(0));
-		}
-		if (order.getItems().size() > 1)
-		{
-			total = addOrderItem(total, order.getItems().get(1));
-		}
-		if (order.getItems().size() > 2)
-		{
-			total = addOrderItem(total, order.getItems().get(2));
-		}
-		if (order.getItems().size() > 3)
-		{
-			throw new UnsupportedOperationException("Cannot process order with more than 4 items.");
-		}
-		total=total.setScale(2, RoundingMode.HALF_UP);
-		return total.toString();
+		order.getItems().stream().forEach(e->addOrderItem(e));
+		this.total=this.total.setScale(2, RoundingMode.HALF_UP);
+		return this.total.toString();
 	}
 
-	private BigDecimal addOrderItem(BigDecimal total, OrderItem item)
+	private void addOrderItem(OrderItem item)
 	{
 		float productPrice = item.getProduct().getPrice();
 		float totalPrice = item.getCount()*productPrice;
-		total = total.add(new BigDecimal(totalPrice));
-		return total;
+		this.total = this.total.add(new BigDecimal(totalPrice));
 	}
 }
